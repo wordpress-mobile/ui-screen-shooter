41780caaefc4a6b0619a3ede58e9a8bc2e60cad0
Sendhil Panchadsaram <sendhilp@gmail.com>
2014-12-05 00:15:57 -0800

    Updating for Polldaddy

--- settings.sh
+++ settings.sh
@@ -1,15 +1,16 @@
-export languages="en_US es fr it ja sv pt-BR nl de id ko ru zh-Hans th zh-Hant tr en-GB"
-export shots_per_simulator=5
+export languages="en_US"
+#export languages="en_US es fr it ja sv pt-BR nl de id ko ru zh-Hans th zh-Hant tr en-GB"
+export shots_per_simulator=4
 
 export tmp_dir="/tmp"
 export screenshots_dir="./screenshots"
-export xcode_project_dir="/path/to/project/folder/"
+export xcode_project_dir="path-to-polldaddy"
 export uiautomation_script="./shoot_the_screens.js"
 
 declare -a simulators=(
-"iPhone 6 (8.1 Simulator)",
-"iPhone 6 Plus (8.1 Simulator)",
-"iPhone 5 (8.1 Simulator)"
-"iPhone 4s (8.1 Simulator)"
-"iPad Retina (8.1 Simulator)"
+  "iPhone 6 (8.1 Simulator)",
+  "iPhone 6 Plus (8.1 Simulator)",
+  "iPhone 5 (8.1 Simulator)"
+  "iPhone 4s (8.1 Simulator)"
+  "iPad Retina (8.1 Simulator)"
 )
--- shoot_the_screens.js
+++ shoot_the_screens.js
@@ -12,92 +12,69 @@ var sleep = function(duration) {
     target.delay(duration);
 }
 
-var username = "theultimatecatblog";
-var password = "xxxxxxxxxxxxxxxxxx";
+var loggedIn = function() {
+  for (var i=0; i<win.elements().length; i++) {
+    if (win.elements()[i].name() == "My Surveys") { 
+      return true;
+    }
+  }
+
+  return false;
+}
+
+var username = "enter-user-name-here";
+var password = "enter-password-here";
 
 // wait for app to load
 sleep(5);
 
 UIALogger.logStart("screenshots");
 
-// logout if needed
-if ( app.tabBar().checkIsValid() ) {
-    app.tabBar().buttons()[2].tap(); sleep(1);
-
-    // need two taps of the back button if there is only one blog added to the app
-    while ( ! win.navigationBar().rightButton().checkIsValid() ) {
-        app.navigationBar().leftButton().tap(); sleep(1);
-    }
-
-    app.navigationBar().rightButton().tap(); sleep(2);
-
-    if (model.match(/iPhone/)) {
-        win.tableViews()[0].cells()[1].tap();
-        app.actionSheet().elements()[1].tap();
-    } else {
-        target.frontMostApp().mainWindow().tableViews()[1].cells()[1].tap();
-        target.frontMostApp().mainWindow().popover().actionSheet().elements()[1].tap();
-    }
-}
-
-sleep(5);
+// TODO: Implement code to log user out.
 
 win.textFields()[0].tap(); sleep(1);
-type(username);
+//type(username);
+win.textFields()[0].setValue(username);
 type("\n");
 type(password);
-win.buttons()[1].tap();
+//win.textFields()[1].setValue(password);
+win.buttons()[0].tap();
+sleep(5); // Wait to login
 
 // Wait for login to complete
-// load reader
-while ( !app.tabBar().checkIsValid() ) { sleep(1); }
-app.tabBar().buttons()[0].tap(); sleep(1);
-app.navigationBar().tapWithOptions({tapOffset:{x:0.95, y:0.55}}); sleep(1);
-win.scrollViews()[0].tableViews()[0].cells()[0].tap();
-
-// wait for reader to load content and take screenshot
-sleep(10);
-captureLocalizedScreenshot("1-reader");
-
-// load notifications
-app.tabBar().buttons()[1].tap();
-
-// wait for notifications to load and then take screenshot
+while (!loggedIn()) { sleep(1); }
+UIALogger.logMessage("Logged In!");
 sleep(5);
-captureLocalizedScreenshot("2-notifications");
+captureLocalizedScreenshot("1-select-surveys");
 
-// load blog details
-app.tabBar().buttons()[2].tap(); sleep(1);
-// this is needed if there is more than one blog
-if ( win.tableViews()[0].visibleCells()[0].name() == 'Words, Whimsy, and the World' ) {
-    win.tableViews()[0].visibleCells()[0].tap(); sleep(1);
-}
+// Select the Surveys
+win.tableViews()[0].cells()[0].tap();
+win.tableViews()[0].cells()[1].tap();
+win.tableViews()[0].cells()[2].tap();
 
-// load comments
-win.tableViews()[0].cells()[2].tap(); sleep(1);
-
-// wait for comments to load and then take screenshot
-sleep(5);
-captureLocalizedScreenshot("3-comments");
-
-// load post editor
-app.navigationBar().leftButton().tap(); sleep(1);
-win.tableViews()[0].cells()[0].tap(); sleep(1);
-for (var i=0; i< win.tableViews()[0].cells().length; i++) {
-  if (win.tableViews()[0].cells()[i].name() == 'Great Perspective on Blogging') {
-    win.tableViews()[0].cells()[i].tap(); sleep(1);
+// Press Save Selection Button
+for (var i=0; i < win.buttons().length; i++) {
+  if (win.buttons()[i].name() == "Save Selections") {
+    UIALogger.logMessage("Saving Selections");
+    win.buttons()[i].tap();
+    break;
   }
 }
-
-// take screenshot for post editor
-captureLocalizedScreenshot("4-posteditor");
-
-app.navigationBar().leftButton().tap(); sleep(1);
-app.navigationBar().leftButton().tap(); sleep(1);
-win.tableViews()[0].cells()[3].tap(); sleep(1);
-
-// wait for stats to load and then take screenshot
 sleep(5);
-captureLocalizedScreenshot("5-stats");
+captureLocalizedScreenshot("2-survey-responses");
 
-UIALogger.logPass("screenshots");
+// Load first survey
+win.tableViews()[1].cells()[0].tap();
+sleep(5);
+captureLocalizedScreenshot("3-survey-intro");
+
+// Press Save Selection Button
+for (var i=0; i < win.buttons().length; i++) {
+  if (win.buttons()[i].name() == "Start Survey »") {
+    win.buttons()[i].tap();
+  }
+}
+sleep(5);
+captureLocalizedScreenshot("4-survey-question-page");
+
+UIALogger.logMessage("Finished screenshots");
